f1d78c5c9166851ecd52ebef4691e44e01dec6a7
sebastian8080 <sebas25211@hotmail.com>
2022-04-21 16:01:51 -0500

    Cambiando logo

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -134,12 +134,12 @@ if(strpos($actual_link, 'localhost') === false){
             <div class="d-flex flex-grow-1">
                 <span class="w-100 d-lg-none d-block pl-4">
                     <a class="navbar-brand" href="{{route('web.index')}}">
-                        <img src="{{asset('casacredito-logo.svg')}}" width="100" height="40" alt="">
+                        <img src="{{asset('img/logo_actualizado2.png')}}" width="65" height="35" alt="">
                         </a>
                 </span>
 
                 <a class="navbar-brand d-none d-lg-inline-block px-4" href="{{route('web.index')}}">
-                    <img src="{{asset('casacredito-logo.svg')}}" width="100" height="40" alt="">
+                    <img src="{{asset('img/logo_actualizado2.png')}}" width="85" height="40" alt="">
                     </a>
                 <div class="w-100 text-right">
                     @if(Route::is('web.index') or Route::is('web.detail') ) 
