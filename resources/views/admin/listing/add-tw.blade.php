c117d5ce77097ec2b334359dc4171f95cd6798bf
sebastian8080 <sebas25211@hotmail.com>
2022-08-01 12:13:14 -0500

    keywords en email de info y developer

--- resources/views/admin/listing/add-tw.blade.php
+++ resources/views/admin/listing/add-tw.blade.php
@@ -106,7 +106,7 @@
             @endif --}}
         </div>
 
-        @if(Auth::user()->role == 'administrator')
+        @if(Auth::user()->id == 15 || Auth::user()->id == 147)
             <div class="gap-4 mt-4 sm:gap-6">
                 {!! Form::label('keywords', 'Keywords', ['class' => 'font-semibold']) !!}
                 {!! Form::textarea('keywords', null, ['class' => $inputs, 'rows' => '3']) !!}
