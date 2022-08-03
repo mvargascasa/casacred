d6a40e39a8dfc4711c6e830315dd012d9971c1dd
sebastian8080 <sebas25211@hotmail.com>
2022-08-03 11:05:31 -0500

    Quitando casa credito de la etiqueta og:title

--- resources/views/services/subs.blade.php
+++ resources/views/services/subs.blade.php
@@ -7,7 +7,7 @@
 
 <meta property="og:url"                content="{{url('servicios/'.$service->slug)}}" />
 <meta property="og:type"               content="website" />
-<meta property="og:title"              content="Casa Credito - {{$service->page_title}}" />
+<meta property="og:title"              content="{{$service->page_title}}" />
 <meta property="og:description"        content="{{$service->page_seocescription}}" />
 <meta property="og:image"              content="{{asset('img/meta-image-social-cc.jpg')}}" />
 
