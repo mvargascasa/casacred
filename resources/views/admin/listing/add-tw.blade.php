34240a7571377f8335592c33e730504567ecc7d8
sebastian8080 <sebas25211@hotmail.com>
2022-08-22 14:09:43 -0500

    Agregando bg-color-400 para ver si funciona clase tailwind

--- resources/views/admin/listing/add-tw.blade.php
+++ resources/views/admin/listing/add-tw.blade.php
@@ -127,7 +127,7 @@
 
             {!! Form::text('listing_title', null, ['class' => $inputs, 'pattern' => '.{50,60}', 'onkeyup' => 'countCharsTitle(this);', 'required']) !!}
             {{-- @endif --}}
-            <div id="div_info_character" class="flex @if(isset($listing) &&  Str::length($listing->listing_title) >= 50 && Str::length($listing->listing_title) <=60) bg-green-300 @else bg-red-300 @endif p-1 mt-2 rounded">
+            <div id="div_info_character" class="flex @if(isset($listing) &&  Str::length($listing->listing_title) >= 50 && Str::length($listing->listing_title) <=60) bg-green-400 @else bg-red-400 @endif p-1 mt-2 rounded">
                 <label style="font-weight: 400">
                     Actual <b id="label_count_title"></b> caracteres. (Mínimo 50 - Máximo 60 caracteres)
                 </label>
@@ -151,7 +151,7 @@
                 La descripción que se mostrara en Google debe contener entre <b>140</b> y <b>155</b> caracteres.
             </div>
             @endif --}}
-            <div id="div_info_character_desc" class="flex @if(isset($listing) &&  Str::length($listing->meta_description) >= 150 && Str::length($listing->meta_description) <= 160) bg-green-300 @else bg-red-300 @endif p-1 mt-2 rounded">
+            <div id="div_info_character_desc" class="flex @if(isset($listing) &&  Str::length($listing->meta_description) >= 150 && Str::length($listing->meta_description) <= 160) bg-green-400 @else bg-red-400 @endif p-1 mt-2 rounded">
                 <label style="font-weight: 400">
                     Actual <b id="label_count_desc"></b> caracteres. (Mínimo 150 - Máximo 160 caracteres)
                 </label>
@@ -525,22 +525,22 @@
 
         function countCharsTitle(object){
             if(object.value.length >= 50 && object.value.length <=60){
-                div_info_character.classList.remove('bg-red-300');
-                div_info_character.classList.add('bg-green-300');
+                div_info_character.classList.remove('bg-red-400');
+                div_info_character.classList.add('bg-green-400');
             } else {
-                div_info_character.classList.remove('bg-green-300');
-                div_info_character.classList.add('bg-red-300');
+                div_info_character.classList.remove('bg-green-400');
+                div_info_character.classList.add('bg-red-400');
             }
             label_count_title.innerHTML = object.value.length;
         }
 
         function countCharsDesc(object){
             if(object.value.length >= 150 && object.value.length <= 160){
-                div_info_character_desc.classList.remove('bg-red-300');
-                div_info_character_desc.classList.add('bg-green-300');
+                div_info_character_desc.classList.remove('bg-red-400');
+                div_info_character_desc.classList.add('bg-green-400');
             } else {
-                div_info_character_desc.classList.remove('bg-green-300');
-                div_info_character_desc.classList.add('bg-red-300');
+                div_info_character_desc.classList.remove('bg-green-400');
+                div_info_character_desc.classList.add('bg-red-400');
             }
             label_count_desc.innerHTML = object.value.length;
         }
