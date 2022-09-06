e5afb34056fed1f62f9128e3b0b62db8f2407477
sebastian8080 <sebas25211@hotmail.com>
2022-09-06 11:52:19 -0500

    quitando el getimagesize -> hacia que la pagina se tarde en cargar de /propiedades

--- resources/views/livewire/proplist.blade.php
+++ resources/views/livewire/proplist.blade.php
@@ -45,7 +45,7 @@
                             $imageVerification = asset('uploads/listing/thumb/600/'.$firstImg[0]); 
                           @endphp
                           <div class="carousel-item @if($iiListing==0) active @endif">
-                            <img loading="lazy" src="{{url('uploads/listing/thumb/600',$firstImg[0]??'')}}" class="d-block w-100" alt="{{$listing->listing_title}}-{{$iiListing++}}">
+                            <img loading="lazy" src="@if(@getimagesize($imageVerification)) {{url('uploads/listing/thumb/600',$firstImg[0]??'')}} @else {{url('uploads/listing/600',$firstImg[0]??'')}} @endif" class="d-block w-100" alt="{{$listing->listing_title}}-{{$iiListing++}}">
                             {{-- @if(@getimagesize($imageVerification)) {{url('uploads/listing/thumb/600',$firstImg[0]??'')}} @else {{url('uploads/listing/600',$firstImg[0]??'')}} @endif --}}
                             {{-- @if(@getimagesize($imageVerification)) {{url('uploads/listing/thumb/600',$firstImg[0]??'')}} @else https://casacredito.com/uploads/listing/600/{{$firstImg}} @endif --}}
                           </div>
@@ -58,7 +58,7 @@
                               {{-- @php
                                   $imageVerification = asset('uploads/listing/thumb/600/'.$img);
                               @endphp --}}
-                              <img loading="lazy" src="@if($imageVerification) {{url('uploads/listing/thumb/600',$img)}} @else {{url('uploads/listing/600',$img)}} @endif" class="d-block w-100" alt="{{$listing->listing_title}}-{{$iiListing++}}">
+                              <img loading="lazy" src="@if(@getimagesize($imageVerification)) {{url('uploads/listing/thumb/600',$img)}} @else {{url('uploads/listing/600',$img)}} @endif" class="d-block w-100" alt="{{$listing->listing_title}}-{{$iiListing++}}">
                               {{-- @if(@getimagesize($imageVerification)) {{url('uploads/listing/thumb/600',$img)}} @else {{url('uploads/listing/600',$img)}} @endif --}}
                               {{-- @if(@getimagesize($imageVerification)) {{url('uploads/listing/thumb/600',$img)}} @else https://casacredito.com/uploads/listing/600/{{$img}} @endif --}}
                             </div>
