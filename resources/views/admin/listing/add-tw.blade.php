aaf3f642d6cf14903fdf3c0524cc208e3e7010b3
sebastian8080 <sebas25211@hotmail.com>
2022-09-08 15:14:28 -0500

    quitando divs de modal para ver si eso ocasiona que no funcione el scroll

--- resources/views/admin/listing/add-tw.blade.php
+++ resources/views/admin/listing/add-tw.blade.php
@@ -550,46 +550,29 @@
         </div>
     </form>
 
-    @if(isset($listing) && $listing->product_code == 1778)
+    {{-- @if(isset($listing) && $listing->product_code == 1778)
     <button class="modal-open">Open Modal</button>
-    @endif
+    @endif --}}
 
-    @isset($listing)
+    {{-- @isset($listing)
     <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
         <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
         
         <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-          
-          {{-- <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-            <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-            </svg>
-            <span class="text-sm">(Esc)</span>
-          </div> --}}
     
-          <!-- Add margin if you want to see some of the overlay behind the modal-->
           <div class="modal-content py-4 text-left px-6">
-            <!--Title-->
             <div class="flex justify-between items-center pb-3">
               <p class="text-2xl font-bold">¿Cuál es la razón por la que se desactiva la propiedad?</p>
-              {{-- <div class="modal-close cursor-pointer z-50">
-                <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                  <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                </svg>
-              </div> --}}
             </div>
                 <input type="text" class="border p-5 rounded-md w-full" name="comment_desact" id="comment_desact" required/>
-        
-                <!--Footer-->
                 <div class="flex justify-center pt-2 mt-2">
                   <button onclick="setcomment({{$listing->id}}, this)" class="px-4 bg-green-300 p-3 rounded-lg text-black-500 hover:bg-green-100 hover:text-black-300 mr-2">Enviar comentario</button>
-                  {{-- <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button> --}}
                 </div>
             
           </div>
         </div>
       </div>
-      @endisset
+      @endisset --}}
 </section>
 
 </main>
@@ -830,14 +813,13 @@
             }
         }
 
-    ///////////////////////////////
-    let openmodal = document.querySelectorAll('.modal-open')
-    for (let i = 0; i < openmodal.length; i++) {
-      openmodal[i].addEventListener('click', function(event){
-    	event.preventDefault()
-    	toggleModal()
-      })
-    }
+    // let openmodal = document.querySelectorAll('.modal-open')
+    // for (let i = 0; i < openmodal.length; i++) {
+    //   openmodal[i].addEventListener('click', function(event){
+    // 	event.preventDefault()
+    // 	toggleModal()
+    //   })
+    // }
     
     // const overlay = document.querySelector('.modal-overlay')
     // overlay.addEventListener('click', toggleModal)
@@ -861,12 +843,12 @@
     // };
     
     
-    function toggleModal () {
-      const body = document.querySelector('body')
-      const modal = document.querySelector('.modal')
-      modal.classList.toggle('opacity-0')
-      modal.classList.toggle('pointer-events-none')
-      body.classList.toggle('modal-active')
-    }
+    // function toggleModal () {
+    //   const body = document.querySelector('body')
+    //   const modal = document.querySelector('.modal')
+    //   modal.classList.toggle('opacity-0')
+    //   modal.classList.toggle('pointer-events-none')
+    //   body.classList.toggle('modal-active')
+    // }
     </script>
 @endsection
