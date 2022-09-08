aa28c88461143ee0b3c313962565b83dd8c42022
sebastian8080 <sebas25211@hotmail.com>
2022-09-08 15:18:11 -0500

    quitando divs de modal para ver si eso ocasiona que no funcione el scroll

--- resources/views/admin/listing/add-tw.blade.php
+++ resources/views/admin/listing/add-tw.blade.php
@@ -558,7 +558,7 @@
     <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
         <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
         
-        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50">
     
           <div class="modal-content py-4 text-left px-6">
             <div class="flex justify-between items-center pb-3">
