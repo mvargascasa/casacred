bc582ee4bc6d7f6f41f8400bf7229a9d9be6a785
sebastian8080 <sebas25211@hotmail.com>
2022-09-28 10:59:16 -0500

    carga del script de facebook pixel mobile

--- resources/views/layouts/webmobile.blade.php
+++ resources/views/layouts/webmobile.blade.php
@@ -30,7 +30,6 @@
 
     <!-- Facebook Pixel Code -->
     <script>
-        setTimeout(() => {
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -41,7 +40,6 @@
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '3081509562095231');
             fbq('track', 'PageView');
-        }, 3000);
     </script>
     <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=3081509562095231&ev=PageView&noscript=1"/></noscript>
     <!-- End Facebook Pixel Code -->   
