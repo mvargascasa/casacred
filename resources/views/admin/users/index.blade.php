cd1c856dd84f9aca08f26e6598ee0e5cea83a6f6
sebastian8080 <sebas25211@hotmail.com>
2022-09-29 12:14:42 -0500

    agregando tr en la lista de users del admin

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,7 +10,8 @@
         <div class="flex flex-col pt-4 px-2">
             <div class="-my-2 py-2 overflow-x-auto">
                 <div class="w-full overflow-scroll mx-auto" style="height: 80vh;">
-                    <table class="w-full whitespace-nowrap bg-white">
+                    <table class="w-full whitespace-nowrap bg-white rounded pt-5">
+                        <tr class="font-semibold"><td>ID</td><td>NOMBRE</td><td>EMAIL</td><td>ROL</td></tr>
                         @foreach($users as $user)
                             <tr>
                                 <td>{{$user->id}}</td><td><a href="{{route('users.edit',$user->id)}}">{{$user->name}}</a></td><td>{{$user->email}}</td><td>{{$user->role}}</td>
