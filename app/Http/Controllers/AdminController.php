f816f205b2f96b78a5c6bea2665aafc529406dbe
sebastian8080 <sebas25211@hotmail.com>
2022-10-07 17:47:16 -0500

    mostrando total de propiedades en el dashboard admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,13 @@ use Illuminate\Support\Facades\DB;
 class AdminController extends Controller
 {     
     public function index(){
-        return view('admin.index');
+        $allproperties = Listing::all();
+        $activatedproperties = Listing::where('status', 1)->get();
+        $availableproperties = Listing::where('available', 1)->get();
+        $totalproperties = $allproperties->count();
+        $totalactivatedproperties = $activatedproperties->count();
+        $totalavailableproperties = $availableproperties->count();
+        return view('admin.index', compact('totalproperties', 'totalactivatedproperties', 'totalavailableproperties'));
     }     
     public function test(){
         return view('admin.test');
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,8 +5,19 @@
 
 @section('content')
 <main class="flex-1 overflow-x-hidden overflow-y-auto">
-    <div class="container mx-auto px-6 py-2">
-        <h3 class="text-gray-700 text-3xl font-medium">Bienvenido </h3>  
+    <div class="mx-auto py-2">
+        {{-- <h3 class="text-gray-700 text-3xl font-medium">Bienvenido</h3>   --}}
+        <div class="flex float-right">
+            <div class="mx-1 border p-2 rounded">
+                <p>Total de Propiedades: <b>{{$totalproperties}}</b></p>
+            </div>
+            <div class="mx-1 border p-2 rounded">
+                <p>Propiedades Activadas: <b>{{$totalactivatedproperties}}</b></p>
+            </div>
+            <div class="mx-1 border p-2 rounded">
+                <p>Propiedades Disponibles: <b>{{$totalavailableproperties}}</b></p>
+            </div>
+        </div>
     </div>
 </main>
 @endsection
