4e3011138efc668bee631b93dc87df62e4754c89
sebastian8080 <sebas25211@hotmail.com>
2022-11-01 15:35:56 -0500

    cambiando direcciones de correo electronico

--- app/Traits/SendEmailTrait.php
+++ app/Traits/SendEmailTrait.php
@@ -8,8 +8,8 @@ trait SendEmailTrait{
     
     public function sendemail(Request $request){
 
-        //$to = "info@casacredito.com,hserrano@casacredito.com,ventas@casacredito.com,asesor1@casacredito.com,asesor2@casacredito.com"; //info@casacredito.com,hserrano@casacredito.com,ventas@casacredito.com
-        $to = "sebas31051999@gmail.com";
+        $to = "info@casacredito.com,hserrano@casacredito.com,ventas@casacredito.com,asesor1@casacredito.com,asesor2@casacredito.com"; //info@casacredito.com,hserrano@casacredito.com,ventas@casacredito.com
+        //$to = "sebas31051999@gmail.com";
         $subject = "Lead Casa Credito - " . strip_tags($request->name) . " | " . date(now());
         switch ($request->interest) {
             case 'Busca Alquiler':
@@ -91,7 +91,7 @@ trait SendEmailTrait{
             'Content-type:text/html;charset=UTF-8' . "\r\n"
         ;
         
-        //mail('sebas31051999@gmail.com', $subject, $message, $header);
+        mail('sebas31051999@gmail.com', $subject, $message, $header);
         return(mail($to,  $subject, $message, $header));
     }
 
