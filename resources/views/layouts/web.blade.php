330471b94e622d9234b391293c82a2141c223f05
sebastian8080 <sebas25211@hotmail.com>
2022-11-02 17:48:22 -0500

    agregando nuevo google tag manager

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -81,7 +81,7 @@ if(strpos($actual_link, 'localhost') === false){
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-N5ZR7NX');</script>
+  })(window,document,'script','dataLayer','GTM-5H6GC9Z');</script>
   <!-- End Google Tag Manager -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -215,8 +215,8 @@ if(strpos($actual_link, 'localhost') === false){
 </head>
 <body>
 
-  <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5ZR7NX"
+ <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5H6GC9Z"
   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 
