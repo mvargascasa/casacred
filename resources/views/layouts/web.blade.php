f53a828880490e9baafce928792db244e46b07ca
sebastian8080 <sebas25211@hotmail.com>
2022-11-02 11:24:37 -0500

    subiendo script de google tag manager

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -75,6 +75,15 @@
 $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if(strpos($actual_link, 'localhost') === false){
 ?>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-N5ZR7NX');</script>
+  <!-- End Google Tag Manager -->
+
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script id="script_analytics" async></script>
 <script>
@@ -216,6 +225,12 @@ if(strpos($actual_link, 'localhost') === false){
 </style>
 </head>
 <body>
+
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5ZR7NX"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
   @php
       $array = [];
       //$categories_navbar = \App\Models\NavbarItems::select('name')->distinct()->get();
