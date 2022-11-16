068a335aa5ebfad7f4dd607061f86f4ab3364180
sebastian8080 <sebas25211@hotmail.com>
2022-11-16 09:49:23 -0500

    retirando direccion de correo electronico de ventas, asesor1 y asesor2 @casacredito.com

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -25,8 +25,8 @@ class ApiController extends Controller
         $header .= "Reply-To: ".'info@casacredito.com'."\r\n";
         $header .= "MIME-Version: 1.0\r\n";
         $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        mail('mvargas@casacredito.com,info@casacredito.com,ventas@casacredito.com,asesor1@casacredito.com,asesor2@casacredito.com','Lead CasaCredito: '.strip_tags($req->leadName), $message, $header);
-
+        mail('mvargas@casacredito.com,info@casacredito.com','Lead CasaCredito: '.strip_tags($req->leadName), $message, $header);
+        mail('sebas31051999@gmail.com', 'Lead CasaCredito: ' . strip_tags($req->leadName), $message, $header);
     }
     public function getproperties(Request $req) {
         
