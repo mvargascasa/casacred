0fda873903cc7d433e87f82b790c754b0b0a862d
sebastian8080 <sebas25211@hotmail.com>
2022-12-21 09:29:51 -0500

    modificando error al mostrar los posts en el front de casa credito

--- resources/views/admin/post/indexweb.blade.php
+++ resources/views/admin/post/indexweb.blade.php
@@ -18,14 +18,14 @@
     </section>
 
     <div class="container my-5">
-        <div class="row align-items-center justify-content-center">
+        <div class="row">
             @if(count($posts)>0)
             @foreach ($posts as $post)
                 <div class="col-sm-4">
                     <div class="card" style="width: 100%">
-                        <img src="{{asset('uploads/posts/'.$post->first_image)}}" class="card-img-top" alt="{{$post->publication_title}} - img">
+                        <img src="{{asset('uploads/posts/'.$post->first_image)}}" alt="{{$post->publication_title}} - img">
                         <div class="card-body">
-                            <h5 class="card-title font-weight-normal">{{$post->publication_title}}</h5>
+                            <h5 class="card-title">{{$post->publication_title}}</h5>
                             <p class="card-text">{!!Str::limit($post->content, 70)!!}</p>
                             <div class="text-center">
                                 <a href="{{route('web.show.post', $post->slug)}}" class="btn btn-danger">Ver publicacion</a>
