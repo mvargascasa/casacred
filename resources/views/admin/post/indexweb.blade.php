17d14226ef4059d2bab7d38b9de08efa3bf2269f
sebastian8080 <sebas25211@hotmail.com>
2022-12-21 10:26:10 -0500

    modificando error al mostrar los posts en el front de casa credito

--- resources/views/admin/post/indexweb.blade.php
+++ resources/views/admin/post/indexweb.blade.php
@@ -22,7 +22,7 @@
             @if(count($posts)>0)
                 @foreach ($posts as $post)
                     <div class="col-12 col-sm-12 col-md-6 col-lg-4">
-                        <div class="card w-100 mb-3 h-100">
+                        <div class="card w-100 h-100 mb-2">
                             <img src="{{asset('uploads/posts/'.$post->first_image)}}" alt="{{$post->publication_title}} - img">
                             <div class="card-body">
                                 <h5 class="card-title">{{$post->publication_title}}</h5>
