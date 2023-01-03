fefd137604625b85b7a9d06268e1f8acc60dcf5b
sebastian8080 <sebas25211@hotmail.com>
2023-01-03 14:36:38 -0500

    timepo de carga en script de google tag manager

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -77,11 +77,15 @@ if(strpos($actual_link, 'localhost') === false){
 ?>
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-5H6GC9Z');</script>
+<script>
+  setTimeout(() => {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5H6GC9Z');
+  }, 3000);
+  </script>
   <!-- End Google Tag Manager -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
