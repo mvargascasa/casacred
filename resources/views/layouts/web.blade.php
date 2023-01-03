b3fe1d2838bedd4c68a62440118af40dee88985c
sebastian8080 <sebas25211@hotmail.com>
2023-01-03 10:29:39 -0500

    cambiando tamaño de imagen logo para seo y velocidad de la pagina

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -256,12 +256,12 @@ if(strpos($actual_link, 'localhost') === false){
             <div class="d-flex flex-grow-1">
                 <span class="w-100 d-lg-none d-block pl-1">
                     <a class="navbar-brand" href="{{route('web.index')}}">
-                        <img src="{{asset('img/LOGO CASA CREDITO_Mesa de trabajo 1.png')}}" width="110" height="45" alt="casa credito inmobiliaria">
+                        <img src="{{asset('img/LOGO CASA CREDITO.png')}}" width="110" height="45" alt="casa credito inmobiliaria">
                     </a>
                 </span>
 
                 <a class="navbar-brand d-none d-lg-inline-block px-4" href="{{route('web.index')}}">
-                    <img src="{{asset('img/LOGO CASA CREDITO_Mesa de trabajo 1.png')}}" width="130" height="50" alt="casa credito inmobiliaria">
+                    <img src="{{asset('img/LOGO CASA CREDITO.png')}}" width="130" height="50" alt="casa credito inmobiliaria">
                     </a>
                 <div class="w-100 text-right">
                     @if(Route::is('web.index') or Route::is('web.detail') ) 
