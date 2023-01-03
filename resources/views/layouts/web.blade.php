0bc48e17c3c4bf76f9e3852351f4554146c9a4b5
sebastian8080 <sebas25211@hotmail.com>
2023-01-03 14:58:49 -0500

    timepo de carga en script de google tag manager

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -83,6 +83,7 @@ if(strpos($actual_link, 'localhost') === false){
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      console.log('cargando gtm 5h6gc9z');
       })(window,document,'script','dataLayer','GTM-5H6GC9Z');
   }, 3000);
   </script>
