09a8af6994646fb7ffa3f1c48c688c72800aa83c
sebastian8080 <sebas25211@hotmail.com>
2023-04-11 16:45:41 -0500

    retirando el list_years cuando consulta a la base de datos por el id

--- resources/views/detailprop.blade.php
+++ resources/views/detailprop.blade.php
@@ -451,14 +451,14 @@
               @if($bathroom>0)<img src="{{asset('img/bathroom-black.png')}}" width="15"><span class="text-danger font-weight-bold small pr-1"> {{$bathroom}} </span> @endif
               @if($garage>0)<img src="{{asset('img/garage-black.png')}}" width="15"><span class="text-danger font-weight-bold small pr-1"> {{$garage}} </span> @endif
             </div> --}}
-            @isset ($listing->listyears)
+            {{-- @isset ($listing->listyears)
               @php
                 $years_construction = DB::table('listing_years')->select('description')->where('id', $listing->listyears)->get();
               @endphp
               <div class="pb-2 d-flex">
                 <p class="pt-1"><b style="font-weight: 500">Años de construcción:</b> <br> {{ $years_construction[0]->description }}</p>
               </div>
-            @endisset  
+            @endisset   --}}
           </div>
 
           @php
