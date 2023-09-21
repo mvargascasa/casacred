8768f1c5ecdb6283d9aaa1bd2c7337ff3969bf55
sebastian8080 <sebas25211@hotmail.com>
2023-09-21 10:50:22 -0500

    retirando plugin de chat

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -427,36 +427,6 @@ if(strpos($actual_link, 'localhost') === false){
 </div> --}}
 {{-- @endif --}}
 
-<!-- Messenger Plugin de chat Code -->
-<div id="fb-root"></div>
-
-<!-- Your Plugin de chat code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
-<script>
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "366818313367457");
-  chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-
-<!-- Your SDK code -->
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v17.0'
-    });
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-    </script>
 <footer>
   <div style="background-color: #2C3144; color: #ffffff">
     <div class="container">
