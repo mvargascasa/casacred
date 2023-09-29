a5a4ed7e97d227473e299b11f6896118a57c5526
sebastian8080 <sebas25211@hotmail.com>
2023-09-29 17:53:07 -0500

    agregando vista con la nueva ruta para visualizar un proyecto

--- resources/views/admin/api-projects/index.blade.php
+++ resources/views/admin/api-projects/index.blade.php
@@ -11,20 +11,22 @@
             <p class="mb-4">Esta visualizando los proyectos de <span class="font-semibold">casapromotora.com</span>.  Para poder editarlos ingrese al <a class="font-semibold" style="color: blue" href="https://casapromotora.com/login" target="_blank">sitio web</a> como administrador </p>
             <section class="grid grid-cols-4 gap-4">
                 @foreach ($projects as $project)
-                <div class="div">
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                        <img class="w-full" src="https://casapromotora.com/uploads/projects/600/{{strtok($project['images'], "|")}}" alt="{{ $project['slug']}}">
-                        <div class="px-6 py-4">
-                          <div class="font-bold text-xl mb-2">{{ $project['project_name'] }}</div>
-                          <p class="text-gray-700 text-base">
-                            {{ $project['state'] }}, {{ $project['city'] }}
-                          </p>
-                        </div>
-                        <div class="px-6 pt-4 pb-2">
-                          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ $project['type']}}</span>
-                        </div>
-                      </div>
-                </div>
+                <a href="{{ route('admin.api.project.show', $project['category_id']) }}">
+                    <div class="div">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img class="w-full" src="https://casapromotora.com/uploads/projects/600/{{strtok($project['images'], "|")}}" alt="{{ $project['slug']}}">
+                            <div class="px-6 py-4">
+                              <div class="font-bold text-xl mb-2">{{ $project['project_name'] }}</div>
+                              <p class="text-gray-700 text-base">
+                                {{ $project['state'] }}, {{ $project['city'] }}
+                              </p>
+                            </div>
+                            <div class="px-6 pt-4 pb-2">
+                              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ $project['type']}}</span>
+                            </div>
+                          </div>
+                    </div>
+                </a>
                 @endforeach
             </section>
         </section>
