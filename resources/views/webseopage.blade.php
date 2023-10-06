29c40671a07786c15edfd052ad6a6be13c52cb43
sebastian8080 <sebas25211@hotmail.com>
2023-10-06 16:36:57 -0500

    cargando funcion de gtag_report_conversion despues de 3seg

--- resources/views/webseopage.blade.php
+++ resources/views/webseopage.blade.php
@@ -15,18 +15,20 @@
 <!-- Event snippet for Botón Whatshap Casa credito conversion page
 In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
 <script>
-  function gtag_report_conversion(url) {
-    var callback = function () {
-      if (typeof(url) != 'undefined') {
-        window.location = url;
-      }
-    };
-    gtag('event', 'conversion', {
-        'send_to': 'AW-11250334200/2UCHCPDGr-kYEPjzyfQp',
-        'event_callback': callback
-    });
-    return false;
-  }
+  setTimeout(() => {
+    function gtag_report_conversion(url) {
+      var callback = function () {
+        if (typeof(url) != 'undefined') {
+          window.location = url;
+        }
+      };
+      gtag('event', 'conversion', {
+          'send_to': 'AW-11250334200/2UCHCPDGr-kYEPjzyfQp',
+          'event_callback': callback
+      });
+      return false;
+    }
+  }, 3500);
 </script>
 
 @livewireStyles
