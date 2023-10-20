4e38334409cb04f25be79abcc6a23beeb7cb030c
sebastian8080 <sebas25211@hotmail.com>
2023-10-20 17:25:52 -0500

    agregando h2 en los listing_title de las card

--- resources/views/livewire/proplist.blade.php
+++ resources/views/livewire/proplist.blade.php
@@ -101,7 +101,7 @@
                   @foreach ($categories as $cat) @if ($cat->slug==$listing->listingtypestatus) {{$cat->status_title}} @endif @endforeach
                 </div>
                 <br>
-                <div class="w-100  font-weight-bold text-truncate"><a class="link-dark link-sindeco" href="{{route('web.detail',$listing->slug)}}">{{$listing->listing_title}}</a></div>
+                <h2 class="w-100 h6 font-weight-bold text-truncate"><a class="link-dark link-sindeco" href="{{route('web.detail',$listing->slug)}}">{{$listing->listing_title}}</a></h2>
               <div class="p-0 small font-weight-bold text-muted">@if(Str::contains($listing->address, ',')){{$listing->address}} @else {{$listing->state}}, {{$listing->city}}, {{$listing->address}}@endif</div>
               <div class="small lh-sm" style="max-height:50px; overflow: hidden;">{{mb_substr(strip_tags($listing->listing_description),0,200)}}...</div>
                 @php
@@ -181,6 +181,110 @@
     </div>
     @endif
  <!-- Fin Propiedad -->   
+
+ <!--new design-->
+
+{{-- @php $ii=0; @endphp
+
+@php
+  if(isset($_SERVER['HTTP_USER_AGENT'])){
+    $useragent= $_SERVER['HTTP_USER_AGENT'];
+    $ismobile=preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|zh-cn|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4));
+  }else{ $ismobile = false; }
+@endphp
+
+@if(count($listings)>0)
+  <section class="row">
+    <h2 style="font-weight: 400; font-size: x-large" class="mb-3">Se encontraron {{ count($listings) }} propiedades</h2>
+    @foreach($listings as $listing)
+    @php $ii++; @endphp
+    <article class="col-sm-4 mb-4">
+      <div class="card rounded-0">
+        <div>
+          <a href="{{route('web.detail', $listing->slug)}}">
+            <div class="d-flex justify-content-center w-100 position-relative">
+              <img class="d-block img-fluid w-100" src="@if(file_exists(public_path().'/uploads/listing/thumb/300/'.strtok($listing->images, "|"))) {{asset('uploads/listing/thumb/300/'.strtok($listing->images, '|'))}} @else {{asset('uploads/listing/300/'.strtok($listing->images, '|'))}} @endif" alt="{{$listing->listing_title}}">
+              <div class="position-absolute d-flex" style="bottom: 10px; left: 10px;">
+              </div>
+            </div>
+          </a>
+          <div>
+            <div class="mt-3 px-3">
+              <h2 class="small">{{ $listing->listing_title}}</h2>
+              <p class="small"><i class="fas fa-map-marker-alt text-danger"></i> {{ $listing->state}}, {{ $listing->city}}, {{$listing->address}}</p>
+              <div class="row">
+                @php
+                  $bedroom=0; //bedroom 41&86&49 //garage 43 //bathroom 48&76&81 // squarefit 44
+                  $bathroom=0;$garage=0;$squarefit=0;
+                  if(!empty($listing->heading_details)){
+                    $allheadingdeatils=json_decode($listing->heading_details); 
+                    foreach($allheadingdeatils as $singleedetails) {
+                      unset($singleedetails[0]);
+                      for($i=1;$i<=count($singleedetails);$i++) { 
+                        if($i%2==0) {  
+                          if($singleedetails[$i-1]==41 || $singleedetails[$i-1]==86 || $singleedetails[$i-1]==49)
+                          { 
+                            if(empty($singleedetails[$i])){ $bedroom+=0; }else{
+                            $bedroom+=$singleedetails[$i]; }
+                          }
+                            if($singleedetails[$i-1]==48 || $singleedetails[$i-1]==76 || $singleedetails[$i-1]==81)
+                          {
+                            if(empty($singleedetails[$i])){ $bathroom+=0; }else{
+                            $bathroom+=$singleedetails[$i]; }
+                          }
+                            if($singleedetails[$i-1]==43)
+                          {
+                            if(empty($singleedetails[$i])){ $garage+=0; }else{
+                              $garage+=$singleedetails[$i]; }
+                          }
+                        }
+                      }
+                      $i++;
+                    }
+									}    
+                @endphp
+                <div class="py-2 d-flex">
+                  @if($listing->construction_area>0)<img src="{{asset('img/house.png')}}" width="15"><span class="text-danger font-weight-bold small pr-2"> {{$listing->construction_area}} m<sup>2</sup> </span> @endif
+                  @if($listing->land_area>0)<img src="{{asset('img/floor.png')}}" width="15"><span class="text-danger font-weight-bold small pr-2"> {{$listing->land_area}} m<sup>2</sup> </span> @endif
+                  @if($bedroom>0)<img src="{{asset('img/bed-black.png')}}" width="15"><span class="text-danger font-weight-bold small pr-2"> {{$bedroom}} </span> @endif
+                  @if($bathroom>0)<img src="{{asset('img/bathroom-black.png')}}" width="15"><span class="text-danger font-weight-bold small pr-2"> {{$bathroom}} </span> @endif
+                  @if($garage>0)<img src="{{asset('img/garage-black.png')}}" width="15"><span class="text-danger font-weight-bold small pr-2"> {{$garage}} </span> @endif
+                </div>
+              </div>
+              <div class="d-flex justify-content-between">
+                <p class="mt-2 h5" style="font-weight: 500">${{ number_format($listing->property_price) }}</p>
+                <p class="px-2 py-1 mt-1">COD: <span class="font-weight-bold">{{ $listing->product_code}}</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer bg-white py-3">
+          <div class="d-flex justify-content-end">
+            <div class="mr-1">
+              <a href="tel:+593983849073" class="btn btn-outline-danger btn-sm px-2 rounded-pill btncall border shadow-sm" style="font-size:13px;"><img width="20px" height="15px" src="{{asset('img/ECUADOR-04.webp')}}" alt=""> Llamar</a>
+            </div>
+            <div class="mx-1">
+              <a href="tel:+17186903740" class="btn btn-outline-danger btn-sm px-2 rounded-pill btncall border shadow-sm" style="font-size:13px;"><img width="20px" height="15px" src="{{asset('img/USA-05.webp')}}" alt=""> Llamar</a>
+            </div>
+            <div class="mx-1">
+              <button class="btn btn-danger btn-sm px-2 d-sm-inline-block rounded-pill shadow-sm" 
+              data-toggle="modal" data-target="#modalContact" style="font-size:13px;" onclick="setInterest('COD {{$listing->product_code}}')" title="Solicitar Información"><i class="fas fa-envelope"></i></button>
+            </div>
+              @php
+                $urlwpp = "https://api.whatsapp.com/send?phone=593983849073&text=Hola, estoy interesado en la propiedad *" . strtoupper($listing->listing_title) . "*. Código: *" . $listing->product_code ."*";
+              @endphp
+              <div class="d-flex align-items-center ml-1">
+                <a onclick="return gtag_report_conversion('{{ $urlwpp }}')" href="https://api.whatsapp.com/send?phone=593983849073&text=Hola, estoy interesado en la propiedad *{{strtoupper($listing->listing_title)}}*. Código: *{{$listing->product_code}}*" class="btn btn-sm rounded-circle border shadow-sm btn-wpp" title="Contactar por Whatsapp"><i class="fab fa-whatsapp" style="font-size: 18px"></i></a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </article>
+    @endforeach
+  </section>
+@endif --}}
+
+<!--end new design-->
  
  @if($listings->count()<6)
  <div class="card row mb-3 justify-center" style="border-top:1px #FA7B34 solid">
