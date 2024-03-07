de4a0d4518ca3480a7e17db1a6155f5ec5f208a3
sebastian8080 <sebas25211@hotmail.com>
2024-03-07 11:18:00 -0500

    cambiando route para editar la propiedad cuando esta bajo la pestaña mis propiedades

--- resources/views/livewire/proplisttw.blade.php
+++ resources/views/livewire/proplisttw.blade.php
@@ -23,7 +23,6 @@
                 {{-- {{route('admin.listings.edit',$propertie->id)}} --}}
                 @if(Auth::user()->role == "user" || Auth::user()->role == "ASESOR")
                     <a href="@if(($url_current == "admin.myproperties" || Route::current()->getName() == "admin.myproperties") && $propertie->property_by == "Housing") {{ route('admin.housing.property.edit', $propertie) }} @elseif(($url_current == "admin.myproperties" || Route::current()->getName() == "admin.myproperties") && $propertie->property_by == "Promotora") {{ route('admin.promotora.property.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() == "admin.myproperties" && $propertie->property_by == "" || $propertie->property_by == "Casa Credito") {{ route('admin.listings.edit', $propertie->id) }} @elseif(Route::currentRouteName() == "admin.properties") {{route('admin.show.listing', $propertie->id)}} @endif" target="_blank">
-                        {{-- @if(Route::current()->getName() == "admin.myproperties" || Auth::user()->role == "administrator") {{ route('admin.listings.edit', $propertie->id) }} @else {{ route('admin.show.listing', $propertie->id) }} @endif --}}
                 @endif
                 
                 <div class="relative">
@@ -34,7 +33,7 @@
                         <a target="_blank" href="@if($url_current == "admin.myproperties" || Route::current()->getName() == "admin.myproperties" && $propertie->property_by == 'Housing') {{ route('admin.housing.property.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() == "admin.myproperties" && $propertie->property_by == "Casa Credito") {{ route('home.tw.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() == "admin.myproperties" && $propertie->property_by == 'Promotora') {{ route('admin.promotora.property.edit', $propertie) }} @else {{ route('admin.show.listing', $propertie) }} @endif"><img class="w-full" src="@if(file_exists(public_path().'/uploads/listing/thumb/600/'.$dirImg)) {{ url('/uploads/listing/thumb/600/', $dirImg) }} @else {{url('uploads/listing/600', $dirImg)}} @endif" alt="{{ $propertie->listing_title}}"></a>
                         {{-- @if(@getimagesize($imageVerification)) {{ url('/uploads/listing/thumb/600/', $dirImg) }} @else {{url('uploads/listing/600', $dirImg)}} @endif --}}
                     @else
-                        <a target="_blank" href="@if($url_current == "admin.myproperties" || Route::current()->getName() =="admin.myproperties" && $propertie->property_by == 'Housing') {{ route('admin.housing.property.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() =="admin.myproperties" && $propertie->property_by == 'Promotora') {{ route('admin.promotora.property.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() =="admin.myproperties" && $propertie->property_by == '' || $propertie->property_by == "Casa Credito") {{route('admin.show.listing', $propertie->id) }} @else {{ route('admin.show.listing', $propertie) }} @endif"><img class="w-full" src="{{ asset('img/sin-imagenes.jpg') }}" alt="Sunset in the mountains"></a>
+                        <a target="_blank" href="@if($url_current == "admin.myproperties" || Route::current()->getName() =="admin.myproperties" && $propertie->property_by == 'Housing') {{ route('admin.housing.property.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() =="admin.myproperties" && $propertie->property_by == 'Promotora') {{ route('admin.promotora.property.edit', $propertie) }} @elseif($url_current == "admin.myproperties" || Route::current()->getName() =="admin.myproperties" && $propertie->property_by == '' || $propertie->property_by == "Casa Credito") {{route('admin.listings.edit', $propertie->id) }} @else {{ route('admin.show.listing', $propertie) }} @endif"><img class="w-full" src="{{ asset('img/sin-imagenes.jpg') }}" alt="Sunset in the mountains"></a>
                         {{-- <div class="absolute top-0 left-0 w-full h-full" style="display: flex; justify-content: center; align-items: center">
                             <p style="color: #ffffff" class="flex">Sin imágenes</p>
                         </div> --}}
@@ -361,7 +360,7 @@
                 @endif
                 <div class="flex justify-center">
                     <a target="_blank" class="btn-edit ml-1 p-1 rounded" style="background-color: #c6f6d5" href="{{ route('home.tw.edit', $s_propertie) }}" style="text-decoration: none">
-                        <p class="text-black text-sm" style="font-weight: 500">Editar propiedad</p>
+                        <p class="text-black text-sm" style="font-weight: 500">Editar propiedad x</p>
                     </a>
                 </div>
                 @endif
