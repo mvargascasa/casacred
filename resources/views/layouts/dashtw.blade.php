2cba0eb25ce9169009756d79f8c31a82539e0100
sebastian8080 <sebas25211@hotmail.com>
2024-04-26 16:44:23 -0500

    cambiando favicon de admin

--- resources/views/layouts/dashtw.blade.php
+++ resources/views/layouts/dashtw.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{asset('favicon-admin.png')}}" type="image/x-icon" />
+    <link rel="icon" href="{{asset('favicon-grupo-housing.png')}}" type="image/x-icon" />
     <link rel="stylesheet" href="{{asset('css/app.css?'.uniqid())}}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
