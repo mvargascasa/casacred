9824018e9597243374e2a74f26e01274f39d259e
sebastian8080 <sebas25211@hotmail.com>
2024-04-30 09:27:11 -0500

    mostrando 4 propiedades en el home

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use App\Models\Parish;
 use App\Models\Post;
 use App\Models\SeoPage;
 use App\Models\Service;
@@ -192,7 +193,7 @@ class WebController extends Controller
         }else{ $ismobile = false; }
         // if($ismobile) return view('indexmobile', compact('states'));
         // else 
-        $listings_outstanding = Listing::where('status', 1)->where('available', 1)->where('outstanding', 1)->take(3)->inRandomOrder()->get();
+        $listings_outstanding = Listing::where('status', 1)->where('available', 1)->where('outstanding', 1)->take(4)->inRandomOrder()->get();
         return view('home', compact('ismobile', 'listings_outstanding'));
     }
 
