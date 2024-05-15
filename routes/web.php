1edd09018d931050841aec9483a096409743c6c4
sayala7986 <sayala7986@gmail.com>
2024-05-15 11:36:02 -0500

    Update routes redirect

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,45 @@ use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WebController::class,'home'])->name('web.index');
+Route::get('/', [WebController::class, 'home'])->name('web.index');
 Route::post('/search-properties', [WebController::class, 'searchHome'])->name('search.home');
 
 Route::get('/{type}-en-{status}{details?}', [PropertyController::class, 'view'])
-     ->name('view.property')
-     ->where('type', '[a-zA-Z-]+')
-     ->where('status', 'general|venta|renta|proyectos') // Asegúrate de que 'status' solo puede ser uno de estos valores predefinidos.
-     ->where('details', '(?:-en-.*)?');
+    ->name('view.property')
+    ->where('type', '[a-zA-Z-]+')
+    ->where('status', 'general|venta|renta|proyectos') // Asegúrate de que 'status' solo puede ser uno de estos valores predefinidos.
+    ->where('details', '(?:-en-.*)?');
+
+
+
+Route::redirect('/propiedades/casas-en-venta-en-cuenca', '/casas-en-venta-en-cuenca', 301);
+
+Route::redirect('/propiedades/departamentos-en-venta-en-cuenca', '/departamentos-en-venta-en-cuenca', 301);
+
+Route::redirect('/propiedades/propiedades/haciendas-en-venta-en-ecuador', '/haciendas-en-venta-en-cuenca', 301);
+
+Route::redirect('/propiedades/terrenos-en-venta-en-cuenca', '/terrenos-en-venta-en-cuenca', 301);
+
+Route::redirect('/propiedades/terrenos-en-venta-en-quito', '/terrenos-en-venta-en-quito', 301);
+
+
+Route::redirect('/propiedades/haciendas-en-venta-en-cuenca', '/haciendas-en-venta-en-cuenca', 301);
+
+
+Route::redirect('/propiedades/locales-comerciales-en-venta-en-ecuador', '/locales-comerciales-en-venta-en-cuenca', 301);
+
+Route::redirect('propiedades/locales-comerciales-en-venta-en-cuenca', '/locales-comerciales-en-venta-en-cuenca', 301);
+
+
+Route::redirect('/propiedades/casas-en-venta-en-quito', '/casas-en-venta-en-quito', 301);
+
+
+
+
+Route::redirect('/propiedades/departamentos-en-alquiler-en-cuenca', '/departamentos-en-renta-en-cuenca', 301);
+
+
+Route::redirect('/propiedades/locales-comerciales-en-alquiler-en-cuenca', '/locales-comerciales-en-renta-en-cuenca', 301);
 
 
 
@@ -40,45 +71,46 @@ Route::post('/send-lead-contact', [WebController::class, 'sendLeadContact'])->na
 //Route::get('/avaluo-de-propiedad', [WebController::class, 'avaluo'])->name('web.avaluo.propiedad');
 
 Route::get('/equipo', [TeamController::class, 'index'])->name('team.index');
-Route::get('/propiedad/{listing:slug}', [WebController::class,'detail'])->name('web.detail');
-Route::get('/getcities/{idState}', [WebController::class,'getcities'])->name('web.getcities');
+Route::get('/propiedad/{listing:slug}', [WebController::class, 'detail'])->name('web.detail');
+Route::get('/getcities/{idState}', [WebController::class, 'getcities'])->name('web.getcities');
 Route::get('/getsector/{city_id}', [WebController::class, 'getsector'])->name('web.getsector');
 Route::get('/getstate/{city}', [WebController::class, 'getstatebycity'])->name('web.getstate');
 Route::get('/getstates/{idCountry}', [WebController::class, 'getstates'])->name('web.states');
-Route::get('/nuestros-servicios', [WebController::class,'serviciosall'])->name('web.nuestros-servicios');
-Route::get('/servicios/notaria-queens-new-york', [WebController::class,'notariausa'])->name('web.notariausa');
-Route::get('/servicios/{service:slug}', [WebController::class,'servicios'])->name('web.servicios');
-Route::get('/servicio/{service:slug}', [WebController::class,'servicio'])->name('web.servicio');
+Route::get('/nuestros-servicios', [WebController::class, 'serviciosall'])->name('web.nuestros-servicios');
+Route::get('/servicios/notaria-queens-new-york', [WebController::class, 'notariausa'])->name('web.notariausa');
+Route::get('/servicios/{service:slug}', [WebController::class, 'servicios'])->name('web.servicios');
+Route::get('/servicio/{service:slug}', [WebController::class, 'servicio'])->name('web.servicio');
 
-Route::get('/politicas-de-privacidad', [WebController::class,'politicas'])->name('web.politicas');
+Route::get('/politicas-de-privacidad', [WebController::class, 'politicas'])->name('web.politicas');
 
-Route::post('sendlead', [WebController::class,'sendlead'])->name('web.sendlead');
+Route::post('sendlead', [WebController::class, 'sendlead'])->name('web.sendlead');
 Route::post('sendcite', [WebController::class, 'sendcite'])->name('web.sendcite');
 Route::post('send-contact-form', [WebController::class, 'sendLeadHome'])->name('send.lead.form.home');
 Route::post('sendleadaval', [WebController::class, 'sendleadaval'])->name('web.sendleadaval');
 Route::post('sendemailinterested', [WebController::class, 'sendemailinterested'])->name('web.send.email.interested');
-Route::get('indextest', [WebController::class,'indextest'])->name('web.indextest');
-Route::get('mobile', [WebController::class,'mobile'])->name('web.mobile');
-Route::get('/mobiledet/{listing:slug}', [WebController::class,'mobiledet'])->name('web.mobiledet');
+Route::get('indextest', [WebController::class, 'indextest'])->name('web.indextest');
+Route::get('mobile', [WebController::class, 'mobile'])->name('web.mobile');
+Route::get('/mobiledet/{listing:slug}', [WebController::class, 'mobiledet'])->name('web.mobiledet');
 
-Route::get('/test88', function () {             });
+Route::get('/test88', function () {
+});
 
-Route::group(['prefix' => 'admin','middleware' => ['auth:sanctum', 'verified']], function () {
-    Route::get('/', [AdminController::class,'index'])->name('admin.index');        
+Route::group(['prefix' => 'admin', 'middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
     Route::resource('listings', ListingController::class, ['as' => 'admin']);
     Route::post('unlocked/{id}', [ListingController::class, 'unlocked'])->name('admin.listings.unlocked');
     //Route::post('delete/{listing_id}', [ListingController::class, 'delete'])->name('admin.listings.delete');    
     Route::post('posted-on-facebook/{listing_id}', [ListingController::class, 'postedfacebook'])->name('admin.listing.posted.facebook');
-    Route::get('reslug/{listing}', [ListingController::class,'reslug'])->name('admin.reslug');
-    Route::get('seo', [ListingController::class,'seo'])->name('admin.seo');
+    Route::get('reslug/{listing}', [ListingController::class, 'reslug'])->name('admin.reslug');
+    Route::get('seo', [ListingController::class, 'seo'])->name('admin.seo');
     Route::get('/show-listing/{listing}', [ListingController::class, 'show_listing'])->name('admin.show.listing');
 
     Route::resource('services', ServiceController::class, ['as' => 'admin']);
-    Route::get('/words', [AdminController::class,'words'])->name('admin.words');    
-    
-    Route::get('tw', [TwController::class,'index'])->name('home.tw');
-    Route::get('tw/create', [TwController::class,'create'])->name('home.tw.create');
-    Route::get('tw/edit/{listing}', [TwController::class,'edit'])->name('home.tw.edit');
+    Route::get('/words', [AdminController::class, 'words'])->name('admin.words');
+
+    Route::get('tw', [TwController::class, 'index'])->name('home.tw');
+    Route::get('tw/create', [TwController::class, 'create'])->name('home.tw.create');
+    Route::get('tw/edit/{listing}', [TwController::class, 'edit'])->name('home.tw.edit');
     Route::get('tw/show/{id}', [TwController::class, 'show'])->name('home.tw.show');
 
     Route::post('setcomment', [TwController::class, 'setcomment'])->name('home.tw.setcomment');
@@ -86,27 +118,31 @@ Route::group(['prefix' => 'admin','middleware' => ['auth:sanctum', 'verified']],
     Route::post('setisinplusvalia', [AdminController::class, 'setisinplusvalia'])->name('home.tw.setisinplusvalia');
     Route::post('setwatermark', [UserController::class, 'quitwatermark'])->name('home.user.watermark');
 
-    Route::get('contacts', [TwController::class,'contacts'])->name('admin.contacts');
-    Route::get('opports', [TwController::class,'opports'])->name('admin.opports');
-    Route::get('properties', [TwController::class,'properties'])->name('admin.properties');
+    Route::get('contacts', [TwController::class, 'contacts'])->name('admin.contacts');
+    Route::get('opports', [TwController::class, 'opports'])->name('admin.opports');
+    Route::get('properties', [TwController::class, 'properties'])->name('admin.properties');
     Route::get('my-properties', [TwController::class, 'properties'])->name('admin.myproperties');
     Route::get('sold-out', [TwController::class, 'properties'])->name('admin.soldout');
-    Route::get('propertieshow/{listing}', [TwController::class,'propertieshow'])->name('admin.propertieshow');
-    Route::get('tw/css', function () { echo filesize($_SERVER["DOCUMENT_ROOT"].'/css/apptw.css'); });
+    Route::get('propertieshow/{listing}', [TwController::class, 'propertieshow'])->name('admin.propertieshow');
+    Route::get('tw/css', function () {
+        echo filesize($_SERVER["DOCUMENT_ROOT"] . '/css/apptw.css');
+    });
 
     Route::get('notifications', [NotificationController::class, 'index'])->name('admin.notifications');
     Route::get('count-notifications', [NotificationController::class, 'countnotifications'])->name('admin.count.notifications');
     Route::get('notification/show/{comment}', [NotificationController::class, 'show'])->name('admin.notification.show');
     Route::post('setviewed', [NotificationController::class, 'setviewed'])->name('admin.comment.setviewed');
-    
-    Route::get('/maps', function () { return view('admin.maps'); });     
-    Route::get('/test', [TwController::class,'test'] )->name('admin.test');
-    
+
+    Route::get('/maps', function () {
+        return view('admin.maps');
+    });
+    Route::get('/test', [TwController::class, 'test'])->name('admin.test');
+
     Route::get('users/search', [UserController::class, 'searchuser'])->name('users.search');
     Route::resource('users', UserController::class);
-    Route::put('users/changepass/{user}', [UserController::class,'changepass'])->name('users.changepass');
-    
-    Route::get('getallimg/{listing}', [AdminController::class,'getallimg'])->name('admin.getallimg');
+    Route::put('users/changepass/{user}', [UserController::class, 'changepass'])->name('users.changepass');
+
+    Route::get('getallimg/{listing}', [AdminController::class, 'getallimg'])->name('admin.getallimg');
 
     Route::get('seo', [SeoController::class, 'index'])->name('admin.seo.index');
     Route::get('seo/pages', [SeoController::class, 'indexpages'])->name('admin.seo.pages.index');
@@ -120,7 +156,7 @@ Route::group(['prefix' => 'admin','middleware' => ['auth:sanctum', 'verified']],
     Route::post('seo/navbar/store', [SeoController::class, 'storenavbar'])->name('admin.seo.navbar.store');
     Route::get('seo/navbar/edit/{id}', [SeoController::class, 'editnavbar'])->name('admin.seo.navbar.edit');
     Route::put('seo/navbar/update/{id}', [SeoController::class, 'updatenavbar'])->name('admin.seo.navbar.update');
-    
+
     Route::get('seo/templates', [SeoController::class, 'indextemplates'])->name('admin.seo.templates.index');
 
     //BLOG
@@ -170,10 +206,10 @@ Route::get('/blog', [WebController::class, 'blog'])->name('web.blog');
 Route::get('/post/{slug}', [WebController::class, 'showpost'])->name('web.show.post');
 
 
-Route::get('/crm/getlistingscsv', [WebController::class,'listingscsv'])->name('web.listingscsv');
+Route::get('/crm/getlistingscsv', [WebController::class, 'listingscsv'])->name('web.listingscsv');
 
 //   SEO
-Route::get('/seo', [WebController::class,'seo'])->name('web.seo');
+Route::get('/seo', [WebController::class, 'seo'])->name('web.seo');
 // Route::get('/casas-de-venta-en-ecuador', [WebController::class,'index'])->name('web.casas.ecu');
 // Route::get('/departamentos-de-venta-en-ecuador', [WebController::class,'index'])->name('web.depar.ecu');
 // Route::get('/terrenos-de-venta-en-ecuador', [WebController::class,'index'])->name('web.terre.ecu');
@@ -199,7 +235,9 @@ Route::get('/guayaquil', [WebController::class, 'index'])->name('web.guayaquil')
 Route::post('/landing/leadcredito', [LandingController::class, 'sendleadcredito'])->name('web.lead.credito');
 Route::get('/landing/solicite-su-credito', [LandingController::class, 'credito'])->name('web.landing.credito');
 
-Route::get('/thank', function(){return view('thank');})->name('thank');
+Route::get('/thank', function () {
+    return view('thank');
+})->name('thank');
 
 Route::get('/properties/category/{type}', [WebController::class, 'getPropertiesByCategory']);
 
