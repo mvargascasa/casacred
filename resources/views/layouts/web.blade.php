ab20fe0f3baf17b418d25d5883f53be345b4280e
sebastian8080 <sebas25211@hotmail.com>
2024-05-16 14:46:05 -0500

    quitando codigo que era para notaria

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -62,21 +62,6 @@
 $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if(strpos($actual_link, 'localhost') === false){
 ?>
-
-    <!-- Hotjar Tracking Code for https://notarialatina.com/ -->
-    <script>
-        setTimeout(() => {
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:4985470,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        }, 3500);
-    </script>
-
     <!-- Google Tag Manager -->
     <script>
         setTimeout(() => {
