7a40dd909c4812f41576d353cec1036e68c96dc0
sayala7986 <sayala7986@gmail.com>
2024-05-17 10:51:07 -0500

    Added hotjar script

--- resources/views/layouts/web2.blade.php
+++ resources/views/layouts/web2.blade.php
@@ -435,7 +435,16 @@ if(strpos($actual_link, 'localhost') === false){
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
     </style>
-
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:4986662,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 </head>
 
 <body>
