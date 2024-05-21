51d88fecc7cac8df4ea38af0d7babc5bef687696
sayala7986 <sayala7986@gmail.com>
2024-05-21 15:40:38 -0500

    Added Tag Convertion Google

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -85,23 +85,8 @@ if(strpos($actual_link, 'localhost') === false){
     <!-- End Google Tag Manager -->
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script id="script_analytics" async></script>
-    <script>
-        setTimeout(() => {
-            document.getElementById('script_analytics').src =
-                'https://www.googletagmanager.com/gtag/js?id=UA-124437679-1';
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-
-            gtag('config', 'AW-806267889'); //    Adwords
-            gtag('config', 'UA-124437679-1'); //  Analytics 
-
-        }, 3500);
-    </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11250334200"></script> 
+    <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11250334200'); </script>
 
     <!-- Facebook Pixel Code -->
     <script>
