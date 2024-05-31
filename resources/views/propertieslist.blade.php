5b7bab0f19e8e5ccbc30e46b94b02d5c53cb4405
sebastian8080 <sebas25211@hotmail.com>
2024-05-31 12:05:00 -0500

    agregando atributo alt a imagenes de propertieslist

--- resources/views/propertieslist.blade.php
+++ resources/views/propertieslist.blade.php
@@ -836,7 +836,7 @@
                     `<li data-target="#carousel${property.id}" data-slide-to="${index}" class="${activeClass}"></li>`;
                 carouselItems += `
         <div class="carousel-item ${activeClass}">
-            <img src="/uploads/listing/600/${image}" class="d-block w-100 carousel-image" style="height:330px" loading="lazy">
+            <img src="/uploads/listing/600/${image}" class="d-block w-100 carousel-image" style="height:330px" loading="lazy" alt="${property.listing_title} - img ${index+1}">
         </div>`;
             });
             let areaInfo = '';
