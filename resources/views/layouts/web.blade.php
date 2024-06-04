ca796924731dbe9fe91c7db07c45e09793dc58d1
sebastian8080 <sebas25211@hotmail.com>
2024-06-04 10:51:31 -0500

    cargando script de hotjar despues de 3,5seg

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -412,14 +412,16 @@ if(strpos($actual_link, 'localhost') === false){
     
     </style>
     <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:4986662,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        setTimeout(() => {
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:4986662,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        }, 3500);
     </script>
 </head>
 
