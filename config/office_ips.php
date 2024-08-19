0b7c1f864eac8302f63fea8efe346f40ec1515c2
sebastian8080 <sebas25211@hotmail.com>
2024-08-19 09:47:13 -0500

    dejando ip predeterminada de la oficina

--- config/office_ips.php
+++ config/office_ips.php
@@ -2,8 +2,6 @@
 
 return [
     'allowed_ips' => [
-        '123.456.789.0', // Ejemplo de IP permitida
-        '98.765.432.1',  // Otra IP permitida
-        '127.0.0.1'
+        '66.231.74.74'
     ],
 ];
