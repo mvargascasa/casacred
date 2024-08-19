80d235efae8a7c31cdeb0624984780039f0912a2
sebastian8080 <sebas25211@hotmail.com>
2024-08-19 09:45:02 -0500

    cambiando direccion ip para pruebas

--- config/office_ips.php
+++ config/office_ips.php
@@ -4,6 +4,6 @@ return [
     'allowed_ips' => [
         '123.456.789.0', // Ejemplo de IP permitida
         '98.765.432.1',  // Otra IP permitida
-        '66.231.74.74'
+        '127.0.0.1'
     ],
 ];
