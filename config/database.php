5c99e125e8d5c01a76435ec2dce3fa278733233d
sebastian8080 <sebas25211@hotmail.com>
2025-03-17 16:15:23 -0500

    feat: delete prefix crm

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => 'crm_',
+            'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
