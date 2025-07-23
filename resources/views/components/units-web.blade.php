7b3a2933f7c5bc9f1f52390f4000f93c4237552f
sebastian8080 <sebas25211@hotmail.com>
2025-07-23 15:11:33 -0500

    feat: add description details in each unit

--- resources/views/components/units-web.blade.php
+++ resources/views/components/units-web.blade.php
@@ -6,26 +6,29 @@
               <div class="card-body p-2">
                   <h6 class="card-title mb-1">{{ $unit->name }}</h6>
                   @if($unit->unit_number)
-                    <small class="text-muted d-block mb-1">Unidad #{{ $unit->unit_number ?? '-' }}</small>
+                    <strong class="text-muted d-block mb-1">Unidad #{{ $unit->unit_number ?? '-' }}</strong>
                   @endif
                   <div class="mb-1">
                       @if($unit->floor)
-                        <small><strong>Piso:</strong> {{ $unit->floor ?? '-' }}</small><br>
+                        <strong>Piso:</strong> {{ $unit->floor ?? '-' }}<br>
                       @endif
                       @if($unit->area_m2)
-                        <small><strong>Área:</strong> {{ $unit->area_m2 ?? '-' }} m²</small><br>
+                        <strong>Área:</strong> {{ $unit->area_m2 ?? '-' }} m²<br>
                       @endif
                       @if($unit->bedrooms)
-                        <small><strong>Hab:</strong> {{ $unit->bedrooms ?? '-' }} </small><br>
+                        <strong>Hab:</strong> {{ $unit->bedrooms ?? '-' }} <br>
                       @endif
                       @if($unit->bathrooms)
-                        <small><strong>Baños:</strong> {{ $unit->bathrooms ?? '-' }}</small><br>
+                        <strong>Baños:</strong> {{ $unit->bathrooms ?? '-' }}<br>
                       @endif
                       @if($unit->price)
                         <small><strong>Precio:</strong> ${{ number_format($unit->price ?? 0, 2) }}</small>
                       @endif
                       @if($unit->description)
-                        <p>{{ $unit->description }}</p>
+                        <div>
+                            <strong>Detalles:</strong>
+                            <p>{{ $unit->description }}</p>
+                        </div>
                       @endif
                   </div>
                   <span class="badge bg-{{ $unit->status === 'available' ? 'success' : 'secondary' }}">
